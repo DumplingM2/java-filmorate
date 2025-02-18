48db062aea06322890180767abf03fc41593052d
Aleksei <andreev@almaz.spb.ru>
2025-02-18 11:33:55 +0300

    Подправил data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,12 @@
-INSERT INTO mpa_ratings (name) VALUES ('G');     -- id=1
-INSERT INTO mpa_ratings (name) VALUES ('PG');    -- id=2
-INSERT INTO mpa_ratings (name) VALUES ('PG-13'); -- id=3
-INSERT INTO mpa_ratings (name) VALUES ('R');     -- id=4
-INSERT INTO mpa_ratings (name) VALUES ('NC-17'); -- id=5
+MERGE INTO mpa_ratings (name) KEY(name) VALUES ('G');
+MERGE INTO mpa_ratings (name) KEY(name) VALUES ('PG');
+MERGE INTO mpa_ratings (name) KEY(name) VALUES ('PG-13');
+MERGE INTO mpa_ratings (name) KEY(name) VALUES ('R');
+MERGE INTO mpa_ratings (name) KEY(name) VALUES ('NC-17');
 
-INSERT INTO genres (name) VALUES ('Комедия');    -- id=1
-INSERT INTO genres (name) VALUES ('Драма');      -- id=2
-INSERT INTO genres (name) VALUES ('Мультфильм');      -- id=3
-INSERT INTO genres (name) VALUES ('Триллер'); -- id=4
-INSERT INTO genres (name) VALUES ('Документальный');     -- id=5
-INSERT INTO genres (name) VALUES ('Боевик'); -- id=6
+MERGE INTO genres (name) KEY(name) VALUES ('Комедия');
+MERGE INTO genres (name) KEY(name) VALUES ('Драма');
+MERGE INTO genres (name) KEY(name) VALUES ('Мультфильм');
+MERGE INTO genres (name) KEY(name) VALUES ('Триллер');
+MERGE INTO genres (name) KEY(name) VALUES ('Документальный');
+MERGE INTO genres (name) KEY(name) VALUES ('Боевик');
